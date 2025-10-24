0da3c2d5d75d1ba116762546a0166df942ce2831
Luizinx-1412 <luizinrobertomelocorreajunior@gmail.com>
2025-10-24 12:04:04 -0300

    quase terminando

--- src/views/TvView.vue
+++ src/views/TvView.vue
@@ -1,39 +1,119 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+import api from '@/plugins/axios'
+import { useGenreStore } from '@/stores/genre';
 
+const genreStore = useGenreStore();
+const genres = ref([])
+const tvShows = ref([])
+
+onMounted(async () => {
+  const response = await api.get('genre/tv/list?language=pt-BR')
+  genres.value = response.data.genres
+  console.log(response.data)
+})
+
+const listTvShows = async (genreId) => {
+  const response = await api.get('discover/tv', {
+    params: {
+      with_genres: genreId,
+      language: 'pt-BR'
+    }
+  })
+  tvShows.value = response.data.results
+  console.log(response.data.results)
+}
+</script>
 
 <template>
-  <h1>Programas de TV</h1>
+
   <ul class="genre-list">
-    <li v-for="genre in genres" :key="genre.id" class="genre-item">
+    <li
+      v-for="genre in genres"
+      :key="genre.id"
+      @click="listTvShows(genre.id)"
+      class="genre-item"
+    >
       {{ genre.name }}
     </li>
   </ul>
+
+  <div class="tv-list">
+    <div v-for="show in tvShows" :key="show.id" class="tv-card">
+      <img
+        :src="`https://image.tmdb.org/t/p/w500${show.poster_path}`"
+        :alt="show.name"
+      />
+      <div class="tv-details">
+        <p class="tv-title">{{ show.name }}</p>
+        <p class="tv-date">{{ show.first_air_date }}</p>
+        <p>{{ show.genre_ids }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
-
 <style scoped>
-  .genre-list {
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    gap: 2rem;
-    list-style: none;
-    padding: 0;
-  }
+.genre-list {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+  list-style: none;
+  margin-bottom: 2rem;
+}
 
-  .genre-item {
-    background-color: #5d6424;
-    border-radius: 1rem;
-    padding: 0.5rem 1rem;
-    align-self: center;
-    color: #fff;
-    display: flex;
-    justify-content: center;
-  }
+.genre-item {
+  background-color: #387250;
+  border-radius: 1rem;
+  padding: 0.5rem 1rem;
+  color: #fff;
+}
 
-  .genre-item:hover {
-    cursor: pointer;
-    background-color: #7d8a2e;
-    box-shadow: 0 0 0.5rem #5d6424;
-  }
+.genre-item:hover {
+  cursor: pointer;
+  background-color: #4e9e5f;
+  box-shadow: 0 0 0.5rem #387250;
+}
+
+.tv-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+
+.tv-card {
+  width: 15rem;
+  height: 32rem;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  box-shadow: 0 0 0.5rem #000;
+}
+
+.tv-card img {
+  width: 100%;
+  height: 20rem;
+  object-fit: cover;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 0.5rem #000;
+}
+
+.tv-details {
+  padding: 0.5rem;
+}
+
+.tv-title {
+  font-size: 1.1rem;
+  font-weight: bold;
+  line-height: 1.3rem;
+  height: 3.2rem;
+}
+
+.tv-date {
+  color: #666;
+  font-size: 0.9rem;
+  margin-bottom: 0.5rem;
+}
+
+
 </style>
